6d2dad1f2a753962f06722762b2cc052697e7c1c
Pedro Vieira <pedro.vieira@phishx.io>
2025-06-03 16:56:11 -0300

    pre-deploy_20250603-16

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 # commit
 DATE=$(date "+%Y%m%d-%H")
 git checkout gh-pages
-cp -R ../phishx-docs.github.io-temp/* ./
+cp -R ../phishx-docs.github.io-temp/dist/* ./
 git add .
 git commit -m deploy_$DATE
 git push https://phishx-docs:$GHDOCSTOKEN@github.com/phishx-docs/phishx-docs.github.io.git gh-pages
