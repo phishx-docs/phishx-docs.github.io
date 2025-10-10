fd4076e87dc2d3bbb5ed599cdb09819dae970ffa
phishx-docs <phishx-docs@users.noreply.github.com>
2025-10-10 14:34:16 -0300

    safe2

---
+++ safesenders.ps1
@@ -0,0 +1,6 @@
+﻿# Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+Remove-Variable AdminUser -ErrorAction SilentlyContinue
+Remove-Variable TestUser -ErrorAction SilentlyContinue
+$AdminUser = "admin-vieira@phishxnew.onmicrosoft.com"
+$TestUser = "pedro.vieira@phishx.io"
+Get-Content .\safesenders.txt -Raw | Invoke-Expression
